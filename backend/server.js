ce7355afa3216c07340ce518be638ce6e101a48a
Neha Pawar <nehapawar2320@gmail.com>
2025-01-16 11:37:10 +0530

    Setting up the backend server and fetch the details from there

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+import userRoutes from './routes/user.route.js'
+import exploreRoutes from './routes/explore.route.js'
+
+dotenv.config();
+
+const app = express();
+app.use(cors())
+
+app.get("/", (req,res) => {
+    res.send("Server is ready")
+})
+
+//Fetch user profiles and repos
+app.use("/api/users", userRoutes)
+//Get the explore page
+app.use("/api/explore", exploreRoutes)
+
+app.listen(5000, () => {
+    console.log('Server started on http://localhost:5000')
+})
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -19,21 +19,27 @@ const HomePage = () => {
 		setLoading(true);
 		try {
 			//fetch the profile info as well as repos
+			//---->> Part commented as we want to send request to our backend.
+			// const userRes = await fetch(`https://api.github.com/users/${username}`, {
+			// 	headers: {
+			// 		authorization : `token ${import.meta.env.VITE_GITHUB_API_KEY}`
+			// 	}
+			// })
+			// //extract body
+			// const userProfile = await userRes.json();
+			// setUserProfile(userProfile);
 
-			const userRes = await fetch(`https://api.github.com/users/${username}`, {
-				headers: {
-					authorization : `token ${import.meta.env.VITE_GITHUB_API_KEY}`
-				}
-			})
-			//extract body
-			const userProfile = await userRes.json();
-			setUserProfile(userProfile);
+			// //fetch repos
+			// const repoRes = await fetch(userProfile.repos_url);
+			// const repos = await repoRes.json();
 
-			//fetch repos
-			const repoRes = await fetch(userProfile.repos_url);
-			const repos = await repoRes.json();
+			const res = await fetch(`http://localhost:5000/api/users/profile/${username}`)
+
+			const {repos, userProfile} = await res.json()
+
 			repos.sort((a,b) => new Date(b.created_at) - new Date(a.created_at));
 			setRepos(repos);
+			setUserProfile(userProfile);
 			// console.log("userProfile: ",userProfile)
 			// console.log("Repos: ", repos)
 
