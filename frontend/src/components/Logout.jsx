588e598188b21100fc3fdbef5367d156b223651a
Neha Pawar <nehapawar2320@gmail.com>
2025-01-17 08:40:07 +0530

    Implementing logout functionality

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,15 +1,31 @@
 import { MdLogout } from "react-icons/md";
+import {useAuthContext} from '../context/AuthContext'
+import toast from "react-hot-toast";
 // TODO Implement Logout functionality
 
 const Logout = () => {
+	const {authUser, setAuthUser} = useAuthContext();
+
+	const handleLogout = async () => {
+		try {
+			const res = await fetch("/api/auth/logout", {
+				credentials: "include"
+			})
+
+			const data = await res.json();
+			setAuthUser(null)
+		} catch (error) {
+			toast.error(error.message)
+		}
+	}
 	return (
 		<>
 			<img
-				src={"https://img.freepik.com/free-vector/smiling-woman-with-braided-hair_1308-174961.jpg?ga=GA1.1.475717472.1736048471&semt=ais_hybrid"}
+				src={authUser?.avatarUrl}
 				className='w-10 h-10 rounded-full border border-gray-800'
 			/>
 
-			<div className='cursor-pointer flex items-center p-2 rounded-lg bg-glass mt-auto border border-gray-800'>
+			<div className='cursor-pointer flex items-center p-2 rounded-lg bg-glass mt-auto border border-gray-800' onClick={handleLogout}>
 				<MdLogout size={22} />
 			</div>
 		</>
