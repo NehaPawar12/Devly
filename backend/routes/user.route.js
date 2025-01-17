a5f10e649a4b6ae75409b3d6c03dcd87f62d3190
Neha Pawar <nehapawar2320@gmail.com>
2025-01-17 11:30:56 +0530

    Get the like information

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,16 @@
 import express from 'express'
-import { getUserProfileAndRepos, likeProfile } from '../controllers/user.controller.js';
+import { getLikes, getUserProfileAndRepos, likeProfile } from '../controllers/user.controller.js';
 import ensureAuthenticated from '../middleware/ensureAuthenticated.js'
+
 
 const router = express.Router();
 
 router.get("/profile/:username", getUserProfileAndRepos)
 
+//fetch the liked data
+router.get("/likes", ensureAuthenticated , getLikes)
+
 //Like profile
-router.post("/like/:username", ensureAuthenticated, likeProfile)
+router.post("/like/:username", ensureAuthenticated , likeProfile)
 
 export default router;
--- frontend/src/components/LikeProfile.jsx
+++ frontend/src/components/LikeProfile.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { FaHeart } from 'react-icons/fa6'
+import  toast  from 'react-hot-toast';
 
-const LikeProfile = () => {
+const LikeProfile = ({userProfile}) => {
 
     const handleLikeProfile = async () => {
         //where we want to send request to backend
-        
+        try {
+
+            const res = await fetch(`/api/users/like/${userProfile.login}`, {
+                method: "POST",
+                credentials: "include"
+            })
+            const data = await res.json();
+
+            if(data.error) throw new Error(data.error);
+            toast.success(data.message)
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
     }
+
+    
 
   return (
    <button className='p-2 text-xs w-full font-medium rounded-md bg-glass border border-blue-400 flex items-center gap-2'
