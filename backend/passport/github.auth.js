607a59c37c643bd0e3f6f4806b0e22a8993fb77c
Neha Pawar <nehapawar2320@gmail.com>
2025-01-17 20:57:26 +0530

    deployment changes

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -20,7 +20,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "https://localhost:5000/api/auth/github/callback"
+  callbackURL: "https://devly-4rju.onrender.com/api/auth/github/callback"
 },
   async function (accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
